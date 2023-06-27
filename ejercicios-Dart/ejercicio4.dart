9308286785afeb781bbed6ffa119c25ba8fb51b9
3229065958 <cdmanrique58@misena.edu.co>
2023-06-27 16:29:03 -0500

    cambios

---
+++ ejercicios-Dart/ejercicio4.dart
@@ -0,0 +1,9 @@
+void main() {
+  //un aprendiz recibe un subcidio base en el sena recibe
+  // una comision de 10%cuanto ganara al mes//
+  double d1 = 100000;
+  double total = d1 / 10;
+  double suma = total + d1;
+  print(total);
+  print('total de la suma con su subcidio es:$suma');
+}

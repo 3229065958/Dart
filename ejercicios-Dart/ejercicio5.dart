7d6aa352d500152a42455af3a3d7b30070bafb35
3229065958 <cdmanrique58@misena.edu.co>
2023-06-27 16:57:25 -0500

    cambios

---
+++ ejercicios-Dart/ejercicio5.dart
@@ -0,0 +1,12 @@
+void main() {
+  int n1 = 2;
+  int n2 = 3;
+  int n3 = 4;
+
+  int c = n1 * n1;
+  int c2 = n2 * n2;
+  int c3 = n3 * n3;
+  print('cuadrado de numero 2:$c');
+  print('cuadrado de nmumero 3:$c2');
+  print('cuadrado de nnumero 4:$c3');
+}
